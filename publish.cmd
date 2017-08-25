117898e2221ecb62c15cea0f8bd655bdb03908ab
JuanAr <juan.arguello@southworks.com>
2017-08-25 18:38:26 -0300

    Updated Bot's code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=juanar2abscode-Web-Deploy.pubxml -p:Password=gXhAAj7KMwo2pdwAQRlc1Ns7g26ogrm4LoepGM21c7BvtZKWqoiisPuEmdTJ
+
